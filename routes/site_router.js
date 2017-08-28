43a024df17a638cacae072682d0f89c4c881c0ec
Balaji Hari <32857@hexaware.com>
2017-08-28 14:43:01 +0530

    Latest code

---
+++ routes/site_router.js
@@ -0,0 +1,227 @@
+'use strict';
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * All rights reserved.
+ *
+ * Handles the site routing and also handles the calls for user registration
+ * and logging in.
+ *
+ * Contributors:
+ *   David Huffman - Initial implementation
+ *   Dale Avery
+ *******************************************************************************/
+var express = require('express');
+var router = express.Router();
+var setup = require('../setup.js');
+var jqupload = require('jquery-file-upload-middleware');
+var fs = require('fs');
+// Load our modules.
+var userManager;
+var chaincode_ops;
+
+// Use tags to make logs easier to find
+var TAG = 'router:';
+
+// ============================================================================================================================
+// Home
+// ============================================================================================================================
+router.get('/', isAuthenticated, function (req, res) {
+    console.log("User already authenticated");
+});
+
+router.get('/:page', function (req, res) {
+	if (req.params.page == 'login') {
+		res.render('login', {layout : null});
+	} else if(req.params.page == 'getDocument'){
+		console.log('Begin getDocument:' );
+    	getDocument(req, res);
+	} else if (req.params.page == 'logout') {
+		req.session.destroy();
+	    res.redirect('/login');
+	}
+	else {
+		res.render(req.params.page, {role : req.session.role, date : formatDate(new Date())});
+	}
+});
+
+function formatDate(date) {
+	  var monthNames = [
+	    "January", "February", "March",
+	    "April", "May", "June", "July",
+	    "August", "September", "October",
+	    "November", "December"
+	  ];
+	  
+	  var daysOfWeek = [
+		"Sunday", "Monday", "Tuesday", "Wednesday",
+		"Thursday", "Friday", "Saturday"               
+	  ];
+	
+	  var day = date.getDate();
+	  var monthIndex = date.getMonth();
+	  var year = date.getFullYear();
+	  var week = date.getDay();
+	  console.log("Getting Date week : " + week);
+	
+	  return daysOfWeek[week] + ', ' + day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+
+// function to create file from base64 encoded string
+function base64_decode(base64str) {
+    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
+    var bitmap = new Buffer(base64str, 'base64');
+    return bitmap;
+}
+
+// convert image to base64 encoded string
+/*var base64str = base64_encode('kitten.jpg');
+console.log(base64str);*/
+
+router.post('/:page', function (req, res) {
+	console.log("Inside POST :" + req.params.page);
+    if (req.body.password) {
+        login(req, res);
+    }else if (req.params.page == 'fileUpload') {
+    	var now = Date.now();
+    	jqupload.fileHandler({
+    		uploadDir: function(){
+    			return './public/uploads/' + now;
+    		},        
+    		uploadUrl: function(){
+    			return '/uploads/' + now;
+    		},
+    	})(req, res);
+    	jqupload.on('begin', function(fileInfo, req, res) {
+    		console.log("beginning upload:" + fileInfo.name +"::"+fileInfo.url);
+    		//fileUpload(fileInfo, req, res);
+    		//fileContent = base64_encode(fileInfo);
+    		//console.log("<<<<<<<<>>>>>>>>>:" + fileContent);
+    	});
+    	jqupload.on('end', function(fileInfo, req, res) {
+    	 console.log("beginning upload:" + fileInfo.name +"::"+fileInfo.url);
+    		var base64Str = base64_encode('./public/uploads/' + now + "/" + fileInfo.name);
+    		chaincode_ops.uploadDocument('WebAppAdmin', req.session.shipmentId, fileInfo.name, 
+    				base64Str, function (err, user) {
+    	        if (err) {
+    	            console.error(TAG, 'Upload failed:', err);
+    	            return res.redirect('/fileUpload');
+    	        } else {
+    	        	console.log('Uploaded Document:' + fileInfo.name + "::"+ now);
+    	        }
+    		})
+    	});
+    	jqupload.on('delete', function (fileInfo, req, res) {
+    		console.log('Delete Document:' + fileInfo.name+"::"+ now);
+    	});
+    	jqupload.on('error', function (e, req, res) {
+            console.log("Error jqupload :" + e.message);
+        });
+    }  else {
+    	register(req, res);
+    }
+});
+
+module.exports = router;
+
+module.exports.setup_helpers = function(configured_chaincode_ops, user_manager) {
+    if(!configured_chaincode_ops)
+        throw new Error('Router needs a chaincode helper in order to function');
+    chaincode_ops = configured_chaincode_ops;
+    userManager = user_manager;
+};
+
+function isAuthenticated(req, res, next) {
+    if (!req.session.username || req.session.username === '') {
+        console.log(TAG, '! not logged in, redirecting to login');
+        return res.redirect('/login');
+    }
+
+    console.log(TAG, 'user is logged in');
+    next();
+}
+
+/**
+ * Handles form posts for registering new users.
+ * @param req The request containing the registration form data.
+ * @param res The response.
+ */
+function register(req, res) {
+    console.log('site_router.js register() - fired');
+    req.session.reg_error_msg = 'Registration failed';
+    req.session.error_msg = null;
+
+    // Determine the user's role from the username, for now
+    console.log(TAG, 'Validating username and assigning role for:', req.body.username);
+    var role = 1;
+    if (req.body.username.toLowerCase().indexOf('auditor') > -1) {
+        role = 3;
+    }
+
+    userManager.registerUser(req.body.username, function (err, creds) {
+        //console.log('! do i make it here?');
+        if (err) {
+            req.session.reg_error_msg = 'Failed to register user:' + err.message;
+            req.session.registration = null;
+            console.error(TAG, req.session.reg_error_msg);
+        } else {
+            console.log(TAG, 'Registered user:', JSON.stringify(creds));
+            req.session.registration = 'Enroll ID: ' + creds.id + '  Secret: ' + creds.secret;
+            req.session.reg_error_msg = null;
+        }
+        res.redirect('/login');
+    });
+}
+
+/**
+ * Handles form posts for enrollment requests.
+ * @param req The request containing the enroll form data.
+ * @param res The response.
+ */
+function login(req, res) {
+    console.log('site_router.js login() - fired');
+    req.session.error_msg = 'Invalid username or password';
+    req.session.reg_error_msg = null;
+
+    // Registering the user against a peer can serve as a login checker, for now
+    console.log(TAG, 'attempting login for:', req.body.username);
+    chaincode_ops.login('WebAppAdmin', req.body.username, req.body.password, function (err, user) {
+        if (err) {
+            console.error(TAG, 'Login failed:', err);
+            return res.redirect('/login');
+        } else {
+        	console.log('obtained user:' + user.Role);
+        	console.log(user.toString());
+        	var userJSON = JSON.parse(user.toString());
+        	req.session.role = userJSON.role;
+        	req.session.username = userJSON.userName;
+        	req.session.shipmentId = '900';
+            return res.redirect('/' + req.body.username + '');
+            //req.session.username = req.body.username;
+            //req.session.name = req.body.username;
+            //req.session.error_msg = null;
+        }
+    });
+}
+
+function getDocument(req, res) {
+	chaincode_ops.fileView('WebAppAdmin', req.session.shipmentId, req.query.file, function (err, fileStr) {
+        if (err) {
+            console.log(TAG, 'Get File failed:', err);
+        } else {
+        	console.log('Got file bytes from blockchain:' + fileStr.toString());
+        	var data = base64_decode(fileStr.toString());
+            res.writeHead(200, {"Content-Type": "image/png"});
+            res.end(data, 'binary');
+        }
+	})
+	console.log("Getting out of get document");
+}
+
---
+++ src/chaincode/lc_cc.go
@@ -0,0 +1,474 @@
+/*
+Copyright IBM Corp 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+type LoginInfo struct {
+	Username	string	`json:"userName"`
+	Password	string	`json:"password"`
+}
+
+type User struct {
+	Username 		string `json:"userName"`
+	Role		 	string `json:"role"`
+	Password		string `json:"password"`
+}
+
+type LC struct {
+	ShipmentId				string	  `json:"shipmentId"`
+	ContentDescription		string	  `json:"contentDesc"`
+	ContentValue			float32	  `json:"contentValue"`
+	ExporterCompany			string	  `json:"exporterCompany"`
+	ExporterBank			string	  `json:"exporterBank"`
+	ImporterCompany			string	  `json:"importerCompany"`
+	ImporterBank			string	  `json:"importerBank"`
+	FreightCompany			string	  `json:"freightCompany"`
+	PortOfLoading			string	  `json:"portOfLoading"`
+	PortOfEntry				string	  `json:"portOfEntry"`
+	currentStatus			string	  `json:"currentStatus"`	
+	DocumentNames			[]string  `json:"documentNames"`
+	ExporterBankApproved	bool	  `json:"exporterBankApproved"`
+	ExporterDocsUploaded	bool	  `json:"exporterDocsUploaded"`
+	CustomsApproved			bool	  `json:"customsApproved"`
+	PaymentComplete			bool	  `json:"paymentComplete"`
+}
+
+// ============================================================================================================================
+// Main
+// ============================================================================================================================
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
+
+// Init resets all the things
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("Init firing. Function will be ignored: " + function)
+
+	// Initialize the collection of commercial paper keys
+	fmt.Println("Initializing user accounts")
+	t.createUser(stub, []string{"importerBank", "importerBank", "Importer Bank"})
+	t.createUser(stub, []string{"customs", "customs", "Customs"})
+	t.createUser(stub, []string{"exporterBank", "exporterBank", "Exporter Bank"})
+	t.createUser(stub, []string{"exporter", "exporter", "Exporter"})
+	
+	fmt.Println("Initializing LC keys collection")
+	var blank []string
+	blankBytes, _ := json.Marshal(&blank)
+	err := stub.PutState("LCKeys", blankBytes)
+	if err != nil {
+		fmt.Println("Failed to initialize LC key collection")
+	}
+	
+	fmt.Println("Initialization complete")
+	return nil, nil
+}
+
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+    // Handle different functions
+    if function == "read" {                            //read a variable
+        return t.read(stub, args)
+    } else if function == "login" {
+		return t.Login(stub, args)
+    } else if function == "fileView" {
+		return t.fileView(stub, args)	
+    }
+    fmt.Println("query did not find func: " + function)
+
+    return nil, errors.New("Received unknown function query: " + function)
+}
+
+func (t *SimpleChaincode) Login(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Inside Login method")
+	var username, password, jsonResp string
+	var err error
+
+    if len(args) < 2 {
+        return nil, errors.New("Incorrect number of arguments. Expecting username and password")
+    }
+    username = args[0]
+		password = args[1]
+
+    valAsbytes, err := stub.GetState(username)
+		if err != nil {
+        jsonResp = "{\"Error\":\"Username not found " + username + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+		var existingUser User
+		json.Unmarshal(valAsbytes, &existingUser)
+
+		if existingUser.Password != password {
+			jsonResp = "{\"Error\":\"Password does not match for " + username + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+		fmt.Println("Login complete")
+    return valAsbytes, nil
+}
+
+func (t *SimpleChaincode) createLC(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Creating LC")
+
+	if len(args) != 1 {
+		fmt.Println("Error obtaining LC JSON string")
+		return nil, errors.New("createLC accepts 1 argument (LCJSONString)")
+	}
+	
+	var lc LC
+	var err error
+	fmt.Println("Unmarshalling LC");
+	
+	err = json.Unmarshal([]byte(args[0]), &lc)
+	if err != nil {
+		fmt.Println("error invalid LC string")
+		return nil, errors.New("Invalid LC string")
+	}
+	
+	lc.currentStatus = "Created"
+	lc.ExporterBankApproved = false
+	lc.ExporterDocsUploaded = false
+	lc.CustomsApproved = false
+	lc.PaymentComplete = false
+	
+	var shipmentId string
+	shipmentId = lc.ShipmentId
+	
+	lcBytes, err := json.Marshal(&lc)
+	if err != nil {
+		fmt.Println("Error marshalling lc")
+		return nil, errors.New("Error marshalling LC")
+	}
+	
+	err = stub.PutState(lc.ShipmentId, lcBytes)
+	if err != nil {
+		fmt.Println("Error creating LC")
+		return nil, errors.New("Error creating LC")
+	}
+
+	fmt.Println("Marshalling account bytes to write")
+
+	// Update the LC keys by adding the new key
+	fmt.Println("Getting LC Keys")
+	keysBytes, err := stub.GetState("LCKeys")
+	if err != nil {
+		fmt.Println("Error retrieving LC keys")
+		return nil, errors.New("Error retrieving LC keys")
+	}
+	var keys []string
+	err = json.Unmarshal(keysBytes, &keys)
+	if err != nil {
+		fmt.Println("Error unmarshel keys")
+		return nil, errors.New("Error unmarshalling LC keys ")
+	}
+
+	fmt.Println("Appending the new key to LC Keys")
+	foundKey := false
+	for _, key := range keys {
+		if key == lc.ShipmentId {
+			foundKey = true
+		}
+	}
+	if foundKey == false {
+		keys = append(keys, lc.ShipmentId)
+		keysBytesToWrite, err := json.Marshal(&keys)
+		if err != nil {
+			fmt.Println("Error marshalling keys")
+			return nil, errors.New("Error marshalling the keys")
+		}
+		fmt.Println("Put state on PaperKeys")
+		err = stub.PutState("LCKeys", keysBytesToWrite)
+		if err != nil {
+			fmt.Println("Error writting keys back")
+			return nil, errors.New("Error writing the keys back")
+		}
+	}
+
+	fmt.Println("LC Creation success", lc);
+	
+	tosend := "LC created successfully for shipmentId :" + shipmentId + "." + stub.GetTxID();
+    err = stub.SetEvent("invokeEvt", []byte(tosend))
+    if err != nil {
+        return nil, err
+    }
+	
+	return nil, nil
+}
+
+func (t *SimpleChaincode) createUser(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Creating user")
+
+	// Obtain the username to associate with the account
+	if len(args) != 3 {
+		fmt.Println("Error obtaining username/password/role")
+		return nil, errors.New("createUser accepts 3 arguments (username, password, role)")
+	}
+	username := args[0]
+	password := args[1]
+	role := args[2]
+
+	// Build an user object for the user
+	var user = User{Username: username, Password: password, Role: role}
+	userBytes, err := json.Marshal(&user)
+	if err != nil {
+		fmt.Println("error creating user" + user.Username)
+		return nil, errors.New("Error creating user " + user.Username)
+	}
+
+	fmt.Println("Attempting to get state of any existing account for " + user.Username)
+	existingBytes, err := stub.GetState(user.Username)
+	if err == nil {
+
+		var existingUser User
+		err = json.Unmarshal(existingBytes, &existingUser)
+		if err != nil {
+			fmt.Println("Error unmarshalling user " + user.Username + "\n--->: " + err.Error())
+
+			if strings.Contains(err.Error(), "unexpected end") {
+				fmt.Println("No data means existing user found for " + user.Username + ", initializing user.")
+				err = stub.PutState(user.Username, userBytes)
+
+				if err == nil {
+					fmt.Println("created user" + user.Username)
+					return nil, nil
+				} else {
+					fmt.Println("failed to create initialize user for " + user.Username)
+					return nil, errors.New("failed to initialize an account for " + user.Username + " => " + err.Error())
+				}
+			} else {
+				return nil, errors.New("Error unmarshalling existing account " + user.Username)
+			}
+		} else {
+			fmt.Println("Account already exists for " + user.Username + " " + existingUser.Username)
+			return nil, errors.New("Can't reinitialize existing user " + user.Username)
+		}
+	} else {
+
+		fmt.Println("No existing user found for " + user.Username + ", initializing user.")
+		err = stub.PutState(user.Username, userBytes)
+
+		if err == nil {
+			fmt.Println("created user" + user.Username)
+			return nil, nil
+		} else {
+			fmt.Println("failed to create initialize user for " + user.Username)
+			return nil, errors.New("failed to initialize an user for " + user.Username + " => " + err.Error())
+		}
+
+	}
+}
+
+func (t *SimpleChaincode) uploadDocument(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value, jsonResp string
+	var err error
+	fmt.Println("running uploadDocument()")
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3. name of the key and value to set")
+	}
+
+	key = args[0]+"_"+args[1]                            //rename for fun
+	value = args[2]
+	err = stub.PutState(key, []byte(value))  //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	
+	//update document detail in LC JSON
+	valAsbytes, err := stub.GetState(args[0])		
+	if err != nil {
+	    jsonResp = "{\"Error\":\"LC JSON not found " + args[1] + "\"}"
+	    return nil, errors.New(jsonResp)
+	}
+	
+	var existingLC LC
+	err = json.Unmarshal(valAsbytes, &existingLC)
+	if err == nil {
+		existingLC.DocumentNames = append(existingLC.DocumentNames, args[2]);
+	}
+	lcBytes, err := json.Marshal(&existingLC)
+	if err != nil {
+		fmt.Println("Error marshalling lc")
+		return nil, errors.New("Error marshalling LC")
+	}
+	
+	err = stub.PutState(existingLC.ShipmentId, lcBytes)
+	if err != nil {
+		fmt.Println("Error updating LC")
+		return nil, errors.New("Error updating LC")
+	}
+	fmt.Println("Successfully updated")
+	
+	return nil, nil
+}
+
+func (t *SimpleChaincode) updateStatus(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+	var key, jsonResp string
+	var value bool
+	fmt.Println("running updateStatus")
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3. updateStatus")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)		
+	if err != nil {
+	    jsonResp = "{\"Error\":\"File not found " + args[1] + "\"}"
+	    return nil, errors.New(jsonResp)
+	}
+	
+	var existingLC LC
+	err = json.Unmarshal(valAsbytes, &existingLC)
+	if err == nil {
+		if args[1] == "ExporterBankApproved" {
+			value, err := strconv.ParseBool(args[2])
+			if err == nil {		
+				existingLC.ExporterBankApproved = value
+				existingLC.currentStatus = "ExporterBankApproved"
+			}
+		} else if args[1] == "ExporterDocsUploaded" {
+			value, err := strconv.ParseBool(args[2])
+			if err == nil {		
+				existingLC.ExporterDocsUploaded = value
+				existingLC.currentStatus = "ExporterDocsUploaded"
+			}
+		} else if args[1] == "CustomsApproved" {
+			value, err := strconv.ParseBool(args[2])
+			if err == nil {
+				existingLC.CustomsApproved = value
+				existingLC.currentStatus = "CustomsApproved"
+			}
+		} else if args[1] == "PaymentComplete" {
+			value, err := strconv.ParseBool(args[2])
+			if err == nil {
+				existingLC.PaymentComplete = value
+				existingLC.currentStatus = "PaymentComplete"
+			}
+		}
+	} else {
+		return nil, errors.New("Error unmarshalling LC")
+	}
+	
+	lcBytes, err := json.Marshal(&existingLC)
+	if err != nil {
+		fmt.Println("Error marshalling lc")
+		return nil, errors.New("Error marshalling LC")
+	}
+	
+	err = stub.PutState(lc.ShipmentId, lcBytes)
+	if err != nil {
+		fmt.Println("Error updating LC")
+		return nil, errors.New("Error updating LC")
+	}
+	fmt.Println("Status successfully updated")
+	return nil, nil
+}
+
+func (t *SimpleChaincode) fileView(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Inside fileView method")
+	var key, jsonResp string
+	var err error
+
+    if len(args) < 2 {
+        return nil, errors.New("Incorrect number of arguments. Expecting shipmentId, fileName")
+    }
+	
+	key = args[0]+"_"+args[1]
+
+    valAsbytes, err := stub.GetState(key)
+	if err != nil {
+	    jsonResp = "{\"Error\":\"File not found " + args[1] + "\"}"
+	    return nil, errors.New(jsonResp)
+	}
+	fmt.Println("FileLoading complete")
+    return valAsbytes, nil
+}
+
+// Invoke is our entry point to invoke a chaincode function
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+	// Handle different functions
+	if function == "init" {//initialize the chaincode state, used as reset
+		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub, args)
+	} else if function == "createUser" {
+		return t.createUser(stub, args)
+	} else if function == "createLC" {
+		return t.createLC(stub, args)	
+	} else if function == "uploadDocument" {
+		fmt.Println("running uploadDocument>>>>>>>>>")
+		return t.uploadDocument(stub, args)	
+	}
+	fmt.Println("invoke did not find func: " + function)					//error
+
+	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0]                            //rename for fun
+	value = args[1]
+	err = stub.PutState(key, []byte(value))  //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+    }
+
+    key = args[0]
+    valAsbytes, err := stub.GetState(key)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    return valAsbytes, nil
+}
